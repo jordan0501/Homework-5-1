b425c2906bcf3bff8ec323af724a9ec57e42ca2e
jordan0501 <a20105511@yahoo.com.tw>
2017-11-25 22:49:49 +0800

    upload

---
+++ P019/source/main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int b[] = { 10, 20, 30, 40 };
+	int *bPtr = b;
+	int i;
+	int offest;
+	printf("Array b printed with:\nArray subscript notation\n");
+	for ( i = 0; i <4 ; i++)
+	{
+		printf("b[%d]=%d\n",i,b[i]);
+	}
+	printf("\nPointer/offest notation where\n"
+		"the pointer is the array name\n");
+	for ( offest = 0; offest < 4; offest++)
+	{
+		printf("*(b+%d)=%d\n",offest,*(b+offest));
+	}
+	printf("\nPointer subscript notation\n");
+	for ( i = 0; i < 4; i++)
+	{
+		printf("bPtr[%d]=%d\n",i,bPtr[i]);
+	}
+	printf("\nPointer/offest nitation\n");
+	for ( offest = 0; offest < 4; offest++)
+	{
+		printf("*(bPtr+%d)=%d\n", offest, *(bPtr + offest));
+	}
+	system("pause");
+	return 0;
+}
